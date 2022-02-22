b98ec10665445e882ab7909e843680d7069e7579
spb71 <shem.baijoo@gmail.com>
2022-02-22 00:02:15 -0500

    Updated repository

--- src/main/java/com/shem/repo/EcommerceRepository.java
+++ src/main/java/com/shem/repo/EcommerceRepository.java
@@ -1,9 +1,11 @@
 package com.shem.repo;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.shem.model.Manager;
 
+@Repository
 public interface EcommerceRepository extends CrudRepository<Manager, Integer> {
 
 }
